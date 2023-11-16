c40cc36e0f817ae22394aedc161ba9e8f942281a
DEEPANRAJ <118879053+Deepan9696@users.noreply.github.com>
2023-11-16 11:55:00 +0530

    Add files via upload

---
+++ email_project/src/services/api.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+
+const API_URI ='https://email-project-backend.vercel.app'
+
+const API_GMAIL = async (serviceUrlObject, requestData = {}, type) => {
+    const { params, urlParams, ...body } = requestData;
+
+    return await axios({
+        method: serviceUrlObject.method,
+        url: `${API_URI}/${serviceUrlObject.endpoint}/${type}`,
+        data: requestData
+    })
+}
+
+export default API_GMAIL;
